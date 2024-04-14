016a7a314e32414fba5e00582599574f07fed0f0
Julien Clément <julienclement22@gmail.com>
2024-04-14 14:34:20 +0200

    chore: Update main script

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import argparse
+from pathlib import Path
+
+import mlflow
+from data import load_data, split_data
+from train import train_model, export_model
+
+
+def main(args):
+    mlflow.autolog()
+
+    with mlflow.start_run():
+        dataset_df = load_data(path=args.training_data)
+        X_train, _, y_train, _ = split_data(df=dataset_df)
+        trained_model = train_model(reg_rate=args.reg_rate, X_train=X_train, y_train=y_train)
+        export_model(trained_model=trained_model, export_directory_path=Path('./results'))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--training_data", dest='training_data', type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    print("\n\n")
+    print("*" * 60)
+
+    parsed_arguments = parse_args()
+    main(parsed_arguments)
+
+    print("*" * 60)
+    print("\n\n")
