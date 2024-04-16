2d353f7ace19206056a4ce37d9bfbbbeebd84eb1
Julien Clément <julienclement22@gmail.com>
2024-04-16 21:11:20 +0200

    feat: Score model in main function

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import mlflow
 from data import load_data, split_data
-from train import train_model, export_model
+from train import train_model, export_model, score_model
 
 
 def main(args):
@@ -11,8 +11,12 @@ def main(args):
 
     with mlflow.start_run():
         dataset_df = load_data(path=args.training_data)
-        X_train, _, y_train, _ = split_data(df=dataset_df)
+        X_train, X_test, y_train, y_test = split_data(df=dataset_df)
         trained_model = train_model(reg_rate=args.reg_rate, X_train=X_train, y_train=y_train)
+
+        test_accuracy = score_model(trained_model=trained_model, X_test=X_test, y_test=y_test)
+        mlflow.log_metric('test_accuracy', test_accuracy)
+
         export_model(trained_model=trained_model, export_directory_path=Path('./results'))
 
 
