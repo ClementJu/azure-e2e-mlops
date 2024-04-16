cf06badb63701b6fe9267d9b7a48e2f7be88d7d5
Julien Clément <julienclement22@gmail.com>
2024-04-16 21:10:42 +0200

    feat: Implement method to score model

--- src/train.py
+++ src/train.py
@@ -9,6 +9,12 @@ def train_model(reg_rate: float, X_train: np.array, y_train: np.array) -> Logist
     return LogisticRegression(C=1 / reg_rate, solver='liblinear').fit(X_train, y_train)
 
 
-def export_model(trained_model: LogisticRegression, export_directory_path: Path = Path('.'), model_name: str = 'sklearn_regression_model.joblib') -> None:
+def score_model(trained_model: LogisticRegression, X_test: np.array, y_test: np.array) -> float:
+    accuracy = trained_model.score(X_test, y_test)
+    return accuracy
+
+
+def export_model(trained_model: LogisticRegression, export_directory_path: Path = Path('.'),
+                 model_name: str = 'sklearn_regression_model.joblib') -> None:
     export_directory_path.mkdir(parents=True, exist_ok=True)
     joblib.dump(value=trained_model, filename=export_directory_path / model_name)
